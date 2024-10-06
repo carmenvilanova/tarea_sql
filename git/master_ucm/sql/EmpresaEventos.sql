bbd8236310777a27726e7f42d48c0f64deafbbd5
Carmen Vilanova de Diego <carmenvilanova01@gmail.com>
2024-10-06 20:53:00 +0200

    Update EmpresaEventos.sql
    pequeños cambios

--- git/master_ucm/sql/EmpresaEventos.sql
+++ git/master_ucm/sql/EmpresaEventos.sql
@@ -20,23 +20,23 @@ DROP TABLE IF EXISTS ubicacion;
 DROP TABLE IF EXISTS asistente;
 
 CREATE TABLE actividad (
-   idActividad char(3),
-   nombre varchar(40) not null,
+   idActividad INT AUTO_iNCREMENT,
+   nombre varchar(60) not null,
    tipo varchar(40) not null,
    coste DECIMAL(10, 2),
    PRIMARY KEY(idActividad)
 );
 
 CREATE TABLE artista (
-   idArtista char(3),
+   idArtista INT AUTO_iNCREMENT,
    nombreArt varchar(40) not null,
    biografia varchar(400) not null,
    PRIMARY KEY(idArtista)
 );
 
 CREATE TABLE actividad_artista (
-   idArtista char(3),
-   idActividad char(3),
+   idArtista INT AUTO_iNCREMENT,
+   idActividad INT,
    cacheArt DECIMAL(10,2),
    PRIMARY KEY(idArtista, idActividad),
    FOREIGN KEY(idArtista) REFERENCES artista(idArtista) ON DELETE cascade,   
@@ -44,10 +44,10 @@ CREATE TABLE actividad_artista (
 );
 
 CREATE TABLE ubicacion (
-   idUbicacion char(3),
+   idUbicacion INT AUTO_iNCREMENT,
    nombreUbi varchar(40) not null,
    direccion varchar(40) not null,
-   tipo varchar(40) not null,
+   tipo varchar(40) not null, /* solo puede ser ciudad o pueblo*/
    caracteristica varchar(40) not null,
    aforo char(5),
    PAlquiler char(5),
@@ -55,9 +55,9 @@ CREATE TABLE ubicacion (
 );
 
 CREATE TABLE evento (
-   idEvento char(3),
-   idUbicacion char(3),
-   idActividad char(3),
+   idEvento INT AUTO_iNCREMENT,
+   idUbicacion INT,
+   idActividad INT,
    nombreUbi varchar(40) not null,
    precio_entrada DECIMAL (10,2),
    PRIMARY KEY(idEvento),
@@ -66,68 +66,59 @@ CREATE TABLE evento (
 );
 
 CREATE TABLE asistente (
-   idAsistente char(3),
-   nombreA varchar(40) not null,
+   idAsistente INT AUTO_iNCREMENT,
+   nombreAs varchar(40) not null,
+   apellidoAs varchar(40) not null,
    email varchar(40) not null,
    PRIMARY KEY(idAsistente)
 );
 
 CREATE TABLE telefono (
-idAsistente char(3),
+idAsistente INT,
 telefono1 char(9),
 telefono2 char(9),
 PRIMARY key(idAsistente)
 );
 
 CREATE TABLE asiste (
-   idAsistente char(3),
-   idEvento char(3),
-   idEntrada char (3),
+   idAsistente INT,
+   idEvento INT,
+   idEntrada INT,
    valoracion char (3),
    PRIMARY KEY(idAsistente,idEvento,idEntrada),
    FOREIGN KEY(idAsistente) REFERENCES asistente(idAsistente) ON DELETE cascade,   
    FOREIGN KEY(idEvento) REFERENCES evento(idEvento) ON DELETE cascade
 );
 
-    -- Trigger para calcular la suma de los cachés de los artistas involucrados en la actividad
-DELIMITER $$
-CREATE TRIGGER calcular_coste_actividad
-AFTER INSERT ON actividad_artista
-FOR EACH ROW
-BEGIN
-    DECLARE totalCoste DECIMAL(10,2);
-    SELECT SUM(aa.cacheArt) INTO totalCoste
-    FROM artista a
-    JOIN actividad_artista aa ON a.idArtista = aa.idArtista
-    WHERE aa.idActividad = NEW.idActividad;
+-- Trigger para calcular la suma de los cachés de los artistas involucrados en la actividad
 
-    -- Actualizar el coste en la tabla actividad
-    UPDATE actividad
-    SET coste = totalCoste
-    WHERE idActividad = NEW.idActividad;
-END$$
-DELIMITER ;
-/
 -- trigger para comprobar aforo
-DELIMITER $$
-CREATE TRIGGER check_aforo BEFORE INSERT ON asiste
-FOR EACH ROW
-BEGIN 
-DECLARE current_asistentes INT;
-DECLARE max_aforo INT;
-
-SELECT num_asistentes,aforo 
-INTO current_asistentes, max_aforo
-FROM evento 
-INNER JOIN ubicacion ON evento.idUbicacion = ubicacion.idUbicacion
-WHERE idEvento = NEW.idEvento;
-
-IF current_asistentes >= max_aforo THEN
-	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Aforo completo. No se pueden vender más entradas";
-    END IF;
-END$$
-DELIMITER ;
 
 -- Se insertan los datos en las tablas
+INSERT INTO actividad (nombre, tipo, coste)
+VALUES 
+('Concierto de Jazz', 'Concierto', 0.00),
+('Exposición de Arte Contemporáneo', 'Exposición', 0.00),
+('Conferencia sobre Inteligencia Artificial', 'Conferencia', 0.00),
+('Obra de Teatro Clásico', 'Teatro', 0.00),
+('Concierto de Música Clásica', 'Concierto', 0.00),
+('Conferencia sobre Economía Circular', 'Conferencia', 0.00),
+('Concierto de Rock and Roll', 'Concierto', 0.00),
+('Exposición Fotográfica', 'Exposición', 0.00),
+('Obra de Teatro Musical', 'Teatro', 0.00),
+('Concierto de Blues', 'Concierto', 0.00);
+
+INSERT INTO artista (nombreArt, biografia)
+VALUES 
+('Carlos Santana', 'Guitarrista y compositor mexicano, reconocido por fusionar el rock con la música latina. Ha ganado múltiples premios Grammy.'),
+('Pablo Picasso', 'Pintor y escultor español, cofundador del movimiento cubista y uno de los artistas más influyentes del siglo XX.'),
+('Beyoncé', 'Cantante y actriz estadounidense, conocida por su poderosa voz y sus contribuciones a la música pop y R&B a nivel global.'),
+('Frida Kahlo', 'Pintora mexicana famosa por sus autorretratos que exploran temas de identidad, poscolonialismo y feminismo.'),
+('Miles Davis', 'Trompetista y compositor estadounidense, considerado uno de los músicos de jazz más innovadores de todos los tiempos.'),
+('Adele', 'Cantante y compositora británica, reconocida por su profunda voz y sus emotivas baladas. Ganadora de varios premios Grammy.'),
+('Vincent van Gogh', 'Pintor posimpresionista neerlandés, conocido por su estilo vibrante y emocional, que dejó un legado inmenso a pesar de su vida trágica.'),
+('John Coltrane', 'Saxofonista y compositor estadounidense, pionero en el desarrollo del jazz modal y conocido por su estilo experimental.'),
+('Lady Gaga', 'Cantante y actriz estadounidense, famosa por su versatilidad en géneros musicales y su estilo artístico único y excéntrico.'),
+('Diego Rivera', 'Muralista mexicano, conocido por sus obras que reflejan la historia y lucha social de México en el siglo XX.');
 
 -- CONSULTAS -- 
